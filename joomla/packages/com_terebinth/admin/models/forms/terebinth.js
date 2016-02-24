a8a2d85e91eaff45ca6731570060e4cbdfe8a3d7
Vincent Wolsink <vincent@byte.nl>
2016-02-24 15:55:04 +0100

    Remove old Joomla plugin. New one is located in seperate repo

--- joomla/packages/com_terebinth/admin/models/forms/terebinth.js
+++
@@ -1,9 +0,0 @@
-jQuery(function() {
-	document.formvalidator.setHandler('terebinthhost',
-		function (value) {
-            regex1=/^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*(:[0-9]{2,5})?$/;
-            regex2=/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(:[0-9]{2,5})?$/;
-			return ( ( regex1.test(value) || regex2.test(value) ) );
-	});
-});
-
